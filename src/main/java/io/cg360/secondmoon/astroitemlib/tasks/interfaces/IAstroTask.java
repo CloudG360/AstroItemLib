e484e7e6c9b69992d307aefa7f24cf4335ccc6f3
CloudGamer360 <16578982+CloudGamer360@users.noreply.github.com>
2019-11-18 20:04:09 +0000

    Pulled a stupid and used @NotNull and forgot to remove it how professional of me wow 100% indeed #cancelled

--- src/main/java/io/cg360/secondmoon/astroitemlib/tasks/interfaces/IAstroTask.java
+++ src/main/java/io/cg360/secondmoon/astroitemlib/tasks/interfaces/IAstroTask.java
@@ -1,13 +1,12 @@
 package io.cg360.secondmoon.astroitemlib.tasks.interfaces;
 
-import com.sun.istack.internal.NotNull;
-
 import java.util.UUID;
 
 public interface IAstroTask extends IKeyedDataProvider {
 
-    @NotNull String getName();
-    @NotNull default String getDescription() { return "No description provided. Yee Haw."; }
+    String getName();
+
+    default String getDescription() { return "No description provided. Yee Haw."; }
 
     boolean isAsync();
 
