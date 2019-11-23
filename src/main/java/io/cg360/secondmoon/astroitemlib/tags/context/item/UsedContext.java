a4a5c746445622de6b18f4b13bc3a119176430c9
CloudGamer360 <16578982+CloudGamer360@users.noreply.github.com>
2019-11-23 09:33:05 +0000

    Pipeline Upgrade: Used Context now supports cancelling.

--- src/main/java/io/cg360/secondmoon/astroitemlib/tags/context/item/UsedContext.java
+++ src/main/java/io/cg360/secondmoon/astroitemlib/tags/context/item/UsedContext.java
@@ -10,18 +10,26 @@ public class UsedContext extends ExecutionContext {
     private HandType handType;
     private ClickType clickType;
 
+    private boolean cancelled;
+
     /**
      *
-     * @param player
-     * @param handType
-     * @param clickType
+     * @param player The player using the item.
+     * @param handType The hand the item is held in.
+     * @param clickType was the use action a punch or a place?
      */
     public UsedContext(Player player, HandType handType, ClickType clickType) {
         super(player);
+
         this.handType = handType;
         this.clickType = clickType;
+        this.cancelled = false;
     }
 
+    public void setCancelled(boolean cancelled) { this.cancelled = cancelled; }
+
+    /** If true, all events connected with the context are cancelled (Like a block change)*/
+    public boolean isCancelled() { return cancelled; }
     public HandType getHandType() { return handType; }
     public ClickType getClickType() { return clickType; }
 }
