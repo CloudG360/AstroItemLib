b49c5d52a3fd190169537d0f43da3d8894464981
CloudGamer360 <16578982+CloudGamer360@users.noreply.github.com>
2019-11-24 19:09:08 +0000

    Pipeline V4 Upgraded BlockInteractContext to disconnect from event

--- src/main/java/io/cg360/secondmoon/astroitemlib/tags/context/blocks/BlockInteractContext.java
+++ src/main/java/io/cg360/secondmoon/astroitemlib/tags/context/blocks/BlockInteractContext.java
@@ -1,17 +1,30 @@
 package io.cg360.secondmoon.astroitemlib.tags.context.blocks;
 
 import io.cg360.secondmoon.astroitemlib.tags.context.ExecutionContext;
+import org.spongepowered.api.block.BlockSnapshot;
 import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.event.block.InteractBlockEvent;
+import org.spongepowered.api.util.Direction;
 
 public class BlockInteractContext extends ExecutionContext {
 
-    private InteractBlockEvent event;
+    private BlockSnapshot targetBlock;
+    private Direction targetSide;
 
-    public BlockInteractContext(Player player, InteractBlockEvent event) {
+    private boolean isCancelled;
+
+    public BlockInteractContext(Player player, BlockSnapshot targetBlock, Direction targetSide, boolean isCancelled) {
         super(player);
-        this.event = event;
+        this.isCancelled = isCancelled;
+
+        this.targetBlock = targetBlock;
+        this.targetSide = targetSide;
     }
 
-    public InteractBlockEvent getEvent() { return event; }
+    public void setCancelled(boolean cancelled) { isCancelled = cancelled; }
+
+    public BlockSnapshot getTargetBlock() { return targetBlock; }
+    public Direction getTargetSide() { return targetSide; }
+
+    public boolean isCancelled() { return isCancelled; }
+
 }
